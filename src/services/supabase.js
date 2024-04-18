41120ea998617387318401b650a4daf84c197ffd
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2024-04-18 21:02:34 +0400

    handling react hook form errors with formStateand add upload image functionality

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oatomqrmxoodjvpeckmg.supabase.co";
+export const supabaseUrl = "https://oatomqrmxoodjvpeckmg.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hdG9tcXJteG9vZGp2cGVja21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5MjIzNTUsImV4cCI6MjAyODQ5ODM1NX0.8BHZqywBx61BEAbc4GnPrH8Zi9MLIu4HIDd4YJrqRio";
 const supabase = createClient(supabaseUrl, supabaseKey);
